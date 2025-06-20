8c34515d0ccbb5bd98b57009a2c4402c889b3bf2
M.a.r.c.o.s <marcossantoosg@gmail.com>
2025-06-20 20:06:53 +0200

    Updated Offsets

--- GLExternal/Roblox/Offsets.hpp
+++ GLExternal/Roblox/Offsets.hpp
@@ -16,7 +16,7 @@ namespace Offsets
 	static uintptr_t IsCoreScript = 0x1a0;
 	static uintptr_t ModuleDetection = 0x1a0 - 0x4;
 
-	static uintptr_t FakeDataModelPointer = 0x675AA38;
+	static uintptr_t FakeDataModelPointer = 0x67C2958;
 	static uintptr_t FakeDataModelToDataModel = 0x1B8;
 
 	static uintptr_t ModuleScriptByteCode = 0x158;
